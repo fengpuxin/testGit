c910bf2c796a98885bfd97e86292d00e4ea2891b
冯普鑫 <fengpuxin@pansky.com.cn>
2019-06-07 10:36:38 +0800

    反复练习暂存区和工作区回退

--- 1.sql
+++ 1.sql
@@ -9,3 +9,9 @@ select * from aa23;
 select * from ac01 a,ac02 b where a.aac001 = b.aac001 and a.aac002 = '610202199001281296';
 
 insert into ac01 select * from ac01 where aac002 = '610202199001281296';
+
+select * from ba23;
+
+select * from ba24;
+
+select 1 from dual;
